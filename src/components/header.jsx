15fe499f8359454778183cc78e9c00fff05ee882
Martijn <mbankert145@gmail.com>
2020-06-01 21:16:06 +0200

    Header image added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Row, Col } from "react-flexbox-grid";
 
 const Header = () => (
   <section className="section">
     <div className="header">
-      <h1>Test</h1>
+      <figure className="header__image-wrapper header__image-wrapper--large"></figure>
+      <Row center="lg" middle="lg" className="header__row">
+        <Col lg={4}>
+          <div className-="header__content">
+            <h2 className="header__title">Welkom.</h2>
+            <span className="header__body">
+              Wij zijn team TNMT. Voor ons project zijn we aan de slag gegaan
+              met de volgende opdracht:
+              <b>
+                "Verbeter de rstaurant ervaring voor blinden/slechtzienden".
+              </b>
+            </span>
+          </div>
+        </Col>
+      </Row>
     </div>
   </section>
 );
