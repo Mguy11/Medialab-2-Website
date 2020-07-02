5bb1821a74657fa0fd04067794bd0a5313a0db75
Martijn <mbankert145@gmail.com>
2020-07-02 14:54:41 +0200

    Last changes concepts block

--- src/components/concepts.jsx
+++ src/components/concepts.jsx
@@ -6,35 +6,40 @@ const Concepts = () => {
   return (
     <section id="concepts" className="section concepts">
       <Row center="lg" className="concepts__row">
-        <div className="concepts__stain concepts__stain--left" alt="blob seven"></div>
+        <Col lg={8}>
+          <div className="concepts__stain concepts__stain--left" alt="blob seven"></div>
 
-        <div className="concepts__content">
-          <h2 className="concepts__title">Concepten</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper. Nullam ornare malesuada euismod.</p>
-        </div>
-
-        <div className="concepts__items">
-          <Concept
-            backgroundImage="../images/buffet_shot.jpg"
-            title="Buffet Concept"
-            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper."
-          />
-          <Concept
-            backgroundImage="../images/buffet_shot.jpg"
-            title="Buffet Concept"
-            body="Lorem ipsum dolo sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper."
-          />
-          <Concept
-            backgroundImage="../images/buffet_shot.jpg"
-            title="Buffet Concept"
-            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper."
-          />
-          <Concept
-            backgroundImage="../images/buffet_shot.jpg"
-            title="Buffet Concept"
-            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper."
-          />
-        </div>
+          <div className="concepts__content">
+            <h2 className="concepts__title">Concepten</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper. Nullam ornare malesuada euismod.</p>
+          </div>
+        </Col>
+        <Col lg={10}>
+          <Row center="lg">
+            <div className="concepts__items">
+              <Concept
+                backgroundImage="../images/buffet_shot.jpg"
+                title="Buffet Concept"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper."
+              />
+              <Concept
+                backgroundImage="../images/buffet_shot.jpg"
+                title="Buffet Concept"
+                body="Lorem ipsum dolo sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper."
+              />
+              <Concept
+                backgroundImage="../images/buffet_shot.jpg"
+                title="Buffet Concept"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper."
+              />
+              <Concept
+                backgroundImage="../images/buffet_shot.jpg"
+                title="Buffet Concept"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sem eu odio luctus eleifend. Aliquam sollicitudin consequat maximus. Nullam tincidunt et elit sit amet semper."
+              />
+            </div>
+          </Row>
+        </Col>
       </Row>
     </section>
   )
